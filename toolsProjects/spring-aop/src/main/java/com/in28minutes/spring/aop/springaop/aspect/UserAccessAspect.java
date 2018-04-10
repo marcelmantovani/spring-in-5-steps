3b5865565b41214f9833e0adb05a26418e8f070e
Marcel Mantovani Filho <37183770+marcelmantovani@users.noreply.github.com>
2018-04-10 08:40:52 -0400

    aspect creation, before method intercept
    the before annotation has syntax of:
    execution (* PACKAGE.*.*(..))

---
+++ toolsProjects/spring-aop/src/main/java/com/in28minutes/spring/aop/springaop/aspect/UserAccessAspect.java
@@ -0,0 +1,22 @@
+package com.in28minutes.spring.aop.springaop.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class UserAccessAspect {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	// What kind of method calls I would intercept?
+	// execution (* PACKAGE.*.*(..))
+	@Before("execution(* com.in28minutes.spring.aop.springaop.business.*.*(..))")
+	public void before (JoinPoint joinPoint) {
+		logger.info("Check for user access");
+		logger.info("User access allowed for method - {}", joinPoint);
+	}
+}
