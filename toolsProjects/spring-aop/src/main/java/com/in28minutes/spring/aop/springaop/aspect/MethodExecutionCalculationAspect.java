5d66dd0476dcc0ec08a69cc7cf82817e787b0cdf
Marcel Mantovani Filho <37183770+marcelmantovani@users.noreply.github.com>
2018-04-10 09:14:34 -0400

    around advice.
    Must return an object otherwise the business returned value is lost.

---
+++ toolsProjects/spring-aop/src/main/java/com/in28minutes/spring/aop/springaop/aspect/MethodExecutionCalculationAspect.java
@@ -0,0 +1,27 @@
+package com.in28minutes.spring.aop.springaop.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class MethodExecutionCalculationAspect {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+
+	@Around("execution(* com.in28minutes.spring.aop.springaop.business.*.*(..))")
+	public Object around (ProceedingJoinPoint joinPoint) throws Throwable {
+		long startTime = System.currentTimeMillis();
+		Object proceed = joinPoint.proceed(); // must use return and pass on, otherwise return of business class is lost
+		long timeTaken = System.currentTimeMillis() - startTime;
+		
+		logger.info(" Time taken of method - {} : {} ", joinPoint, timeTaken);
+		
+		return proceed;
+		
+	}
+}
