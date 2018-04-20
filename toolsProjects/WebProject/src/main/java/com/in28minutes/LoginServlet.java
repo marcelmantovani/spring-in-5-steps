8371db25a4d7498696e16911cbb7ff6478bd2294
Marcel Mantovani Filho <37183770+marcelmantovani@users.noreply.github.com>
2018-04-20 07:55:33 -0400

    redirected the servlet to a jsp

--- toolsProjects/WebProject/src/main/java/com/in28minutes/LoginServlet.java
+++ toolsProjects/WebProject/src/main/java/com/in28minutes/LoginServlet.java
@@ -15,15 +15,8 @@ public class LoginServlet extends HttpServlet{
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException{
 		
-		PrintWriter writer = response.getWriter();
-		writer.println("<html>");
-		writer.println("<head>");
-		writer.println("<title>This is the title</title>");
-		writer.println("</head>");
-		writer.println("<body>");
-		writer.println("this is the content of your page");
-		writer.println("</body>");
-		writer.println("</html>");
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+		
 		
 	}
 
