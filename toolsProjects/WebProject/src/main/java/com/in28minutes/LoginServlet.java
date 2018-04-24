2a844cbe63bf2a216c368ca3a891a1e98354aac3
Marcel Mantovani <37183770+marcelmantovani@users.noreply.github.com>
2018-04-24 10:02:29 -0400

    completed user validation service

--- toolsProjects/WebProject/src/main/java/com/in28minutes/LoginServlet.java
+++ toolsProjects/WebProject/src/main/java/com/in28minutes/LoginServlet.java
@@ -11,14 +11,33 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns="/login.do")
 public class LoginServlet extends HttpServlet{
 	
+	private UserValidationService userValidationService =  new UserValidationService();
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException{
 		
-		request.setAttribute("name", request.getParameter("name"));
-		request.setAttribute("password", request.getParameter("password"));
 		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
 		
 		
 	}
-
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException{
+		
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+		if (userValidationService.isUserValid(name, password)) {
+			request.setAttribute("name", name);
+			request.setAttribute("password", password);
+			
+			request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(request, response);	
+		}
+		else {
+			request.setAttribute("errorMessage", "Invalid Credentials!");
+			request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+		}
+		
+		
+		
+		
+	}
 }
--- toolsProjects/WebProject/src/main/webapp/WEB-INF/views/login.jsp
+++ toolsProjects/WebProject/src/main/webapp/WEB-INF/views/login.jsp
@@ -4,11 +4,14 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>JSP Title</title>
+<title>Loging Page</title>
 </head>
 <body>
 <form action="login.do" method="post">
-Enter your name <input type="text" name="name"/> <input type="submit" value="Login">
+<p>${errorMessage }</p>
+Enter your name <input type="text" name="name"/> <br>
+Enter your password <input type="password" name="password"/> <br>
+<input type="submit" value="Login">
 </form>
 </body>
 </html>
